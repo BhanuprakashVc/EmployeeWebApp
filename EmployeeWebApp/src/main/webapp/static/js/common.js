3b50c30c93d4bfe682d1002a8b1a217728a96f88
BhanuprakashVc <vc.bhanuprakash@gmail.com>
2016-04-18 19:34:38 +0530

    Updated common.js with needed functions

--- EmployeeWebApp/src/main/webapp/static/js/common.js
+++ EmployeeWebApp/src/main/webapp/static/js/common.js
@@ -20,7 +20,6 @@ $(document).ready(function() {
 	$('#bt-update').click(function(){
 		alert("Update button is clicked");
 		document.getElementById('operation').value='update';
-		document.flow.action = 'update';
 		document.flow.submit();
 	});
 	
@@ -30,8 +29,61 @@ $(document).ready(function() {
 		document.flow.submit();
 	});
 	
+	$('#updateButton').click(function(){
+		alert('update button is clicked');
+		console.log('update button is clicked');
+		document.getElementById('operation').value='update';
+		var actionChoosen = fetchSelectedValues();
+		document.showAllFlow.action=actionChoosen;
+		document.showAllFlow.submit();
+	});
+	
+	$('#deleteButton').click(function(){
+		alert('delete button is clicked');
+		console.log('delete button is clicked');
+		document.getElementById('operation').value='delete';
+		var actionChoosen = fetchSelectedValues();
+		document.showAllFlow.action=actionChoosen;
+		document.showAllFlow.submit();
+	});
+	
+	$('#bt-showAll').click(function(){
+		alert('showAll button is clicked');
+		console.log('showAll button is clicked');
+		document.flow.action="showAll";
+		document.flow.method="GET";
+		document.flow.submit();
+	});
+	
+	$('#homeButton').click(function(){
+		alert('home button is clicked');
+		console.log('home button is clicked');
+		document.showAllFlow.action="welcome";
+		document.showAllFlow.method="GET";
+		document.showAllFlow.submit();
+	});
+	
+	function fetchSelectedValues(){
+		var allDataValues = [];
+		var idVal = $('input[type=radio]:checked').closest('tr').find('.data').each(function(){
+			var value = $(this).html();
+			allDataValues.push(value);
+			console.log('td value: '+value);
+		});
+		console.log('allDataValues: ' +allDataValues);
+		var loggedInUserCaasId = allDataValues.pop();
+		var address = allDataValues.pop();
+		var lastName  = allDataValues.pop();
+		var firstName = allDataValues.pop();
+		var id = allDataValues.pop();
+		var actionChoosen = "showAll/"+id + '/'+firstName + '/' + lastName + '/' + address + '/' +loggedInUserCaasId;
+		console.log('actionChoosen: ' +actionChoosen);
+		return actionChoosen;
+	}
 	
 });
+
+
 
 function submitCreateEmp()
 {
@@ -49,19 +101,24 @@ function cancelCreateEmp()
 
 function submitUpdateEmp(){
 	alert("submitUpdateEmp is clicked");
-	document.updateForm.action="update";
-	document.updateForm.submit();
+	document.getElementById('operation').value='update';
+	document.updateEmp.submit();
 }
-
 
 function cancelUpdateEmp(){
-	alert("cancelUpdateEmp is clicked");
-	document.updateForm.action="welcome";
-	document.updateForm.submit();
+	alert("cancelUpdateEmp is clicked ");
+	document.getElementById('operation').value='cancel';
+	document.updateEmp.submit();
 }
 
-function goToShowAllPage(){
-	alert("goToShowAllPage button is clicked");
-	document.msgUpdated.action="showAll";
-	document.msgUpdated.submit();
+function submitDeleteEmp(){
+	alert("submitDeleteEmp is clicked ");
+	document.getElementById('operation').value='delete';
+	document.deleteEmp.submit();
+}
+
+function cancelDeleteEmp(){
+	alert("cancelDeleteEmp is clicked ");
+	document.getElementById('operation').value='cancel';
+	document.deleteEmp.submit();
 }
